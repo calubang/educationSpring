75b2c5a21fe0af45b233e5c90cf6b4626cbb0260
calubang <acalubang@gmail.com>
2019-07-19 14:05:14 +0900

    19.07.19 안병욱 교육내용 최종

--- programmers/src/programmers/Test1.java
+++ programmers/src/programmers/Test1.java
@@ -1,5 +1,8 @@
 package programmers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test1 {
 	public static void main(String[] args) {
 		String answer = "";
@@ -9,5 +12,28 @@ public class Test1 {
 		answer = s.substring( (int)(length/2.0 - 0.5), length/2 + 1);
 		System.out.println(answer);
 		
+		
+		
+		List<String> likeList = new ArrayList<String>();
+		List<String> parseLikeList = new ArrayList<String>();
+		
+		for(String str : likeList) {
+			if(str != null && str.length() <= 2 ) {
+				parseLikeList.add(str);
+			}else if(str != null && str.length() > 2){
+				for(int i=str.length() -1 ; i > 2 ; i--) {
+					parseLikeList.add(str.substring(0, i));
+				}
+			}else {
+				
+			}
+		}
+		String temp = "";
+		StringBuffer sb = new StringBuffer();
+		for(String str : parseLikeList) {
+			sb.append(str+"|");
+		}
+		sb.substring(0, sb.length()-1);
+
 	}
 }
